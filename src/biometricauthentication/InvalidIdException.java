fcbc8c28b2e132d65032cb947714902df2d67d80
sravani <sravania2002@gmail.com>
2024-03-15 15:36:05 +0530

    Biometric Login Successfully

---
+++ src/biometricauthentication/InvalidIdException.java
@@ -0,0 +1,15 @@
+package biometricauthentication;
+
+public class InvalidIdException extends Exception{
+    String code;
+
+    public InvalidIdException(String errorCode, String message){
+        super(message);
+        this.code=errorCode;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+}
